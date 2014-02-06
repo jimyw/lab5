67073006bbb4284e25e31f585ec6d167ecdbdeaa
JimW <abidingsailor@gmail.com>
2014-02-05 21:25:21 -0800

    putting it together

--- public/js/hci-friends.js
+++ public/js/hci-friends.js
@@ -10,11 +10,26 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Javascript connected!");
+
+    // add additional listeners
+    $("h3").click(nameClick);
+}
+
+function nameClick(e) {
+    // Cancel the default action, which prevents the page from reloading
+    e.preventDefault();
+ 
+    // In an event listener, $(this) is the leement that fired the event
+//    var currName = $(this).find("h3").text();   // $(this) points to the <a> tag, which is what the eventhandler is on
+    var currName = $(this).text();  // changed listener to point to the direct object
+    var anagName = anagrammedName(currName);
+
+    // replaces text with the following anagName
+    $(this).text(anagName);
 }
 
 function anagrammedName(name) {
 	// Thanks, Internet Anagram Server!
-	
 	if (name == "Doug Engelbart") {
 		return "Notable Grudge";
 	} 
--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,14 @@
 var data = require("../data.json");
 
 exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+    var name = req.query.name;
+    var description = req.query.description;
+    var newFriend = {
+            "name": name,
+            "description": description,
+            "imageURL": "http://lorempixel.com/400/400/people"
+        };
+//    console.log(newFriend);
+    data["friends"].push(newFriend);    // add newFriend to the friends database
+    res.render('add',data);
+};
